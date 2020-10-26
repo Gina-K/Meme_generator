10d0b3454ee0c927bf84b0019ab1e67be1c9feed
Gina <e.kurmeneva@gmail.com>
2020-10-26 14:04:49 +0300

    Create an onChange handler method

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -9,6 +9,7 @@ class MemeGenerator extends Component {
             randomImage: "http://i.imgflip.com/1bij.jpg",
             allMemeImgs: []
         }
+        this.handlerChange = this.handlerChange.bind(this);
     }
 
     componentDidMount() {
@@ -20,6 +21,11 @@ class MemeGenerator extends Component {
             })
     }
 
+    handlerChange(event) {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+    }
+
     render() {
         return (
             <div>
@@ -29,17 +35,23 @@ class MemeGenerator extends Component {
                         name="topText"
                         placeholder="Enter a top text..."
                         value={this.state.topText}
-                        onChange={this.changeHandler}/>
+                        onChange={this.handlerChange}/>
 
                     <input
                         type="text"
                         name="bottomText"
                         placeholder="...and a bottom text"
                         value={this.state.bottomText}
-                        onChange={this.changeHandler}/>
+                        onChange={this.handlerChange}/>
 
                     <button>Gen</button>
                 </form>
+
+                <div className="meme">
+                    <img src={this.state.randomImage} alt="" />
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
+                </div>
             </div>
         );
     }
