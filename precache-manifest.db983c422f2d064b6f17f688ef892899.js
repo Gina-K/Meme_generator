cb97ea44cbb095960689adbdeca7d958c2666d52
Gina <e.kurmeneva@gmail.com>
2020-10-26 20:09:48 +0300

    Updates

---
+++ precache-manifest.db983c422f2d064b6f17f688ef892899.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41fecac4469b67516e9e11913021fd8e",
+    "url": "/Meme_generator/index.html"
+  },
+  {
+    "revision": "03d918c01366fa0e7a17",
+    "url": "/Meme_generator/static/js/2.022193f9.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Meme_generator/static/js/2.022193f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d126807292e46ac27a33",
+    "url": "/Meme_generator/static/js/main.1083ced4.chunk.js"
+  },
+  {
+    "revision": "8af8a5e08aa1c9676427",
+    "url": "/Meme_generator/static/js/runtime-main.39ffacbe.js"
+  }
+]);
